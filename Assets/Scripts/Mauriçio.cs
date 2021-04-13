434af3a1b85f8935dd19a5e34bd24e6df6576913
GCidon <38065027+GCidon@users.noreply.github.com>
2021-04-13 18:18:50 +0200

    Patrones, mauriçio a ritmo e input

--- Assets/Scripts/Mauriçio.cs
+++ Assets/Scripts/Mauriçio.cs
@@ -1,6 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public class Pair<T, U>
+{
+    public Pair() { }
+    public Pair(T first, U second)
+    {
+        this.First = first;
+        this.Second = second;
+    }
+
+    public T First { get; set; }
+    public U Second { get; set; }
+}
 
 public class Mauriçio : MonoBehaviour
 {
@@ -10,11 +24,15 @@ public class Mauriçio : MonoBehaviour
     public GameObject arrowR_;
     public GameObject arrowU_;
     public GameObject arrowD_;
+    public Text textPhase_;
     public int bpm_;
 
     public Transform commandBar_;
 
     float timer_;
+
+    List<Pair<int, float>> arrowsMauricio_ = new List<Pair<int, float>>();
+    List<Pair<int, float>> arrowsPlayer_ = new List<Pair<int, float>>();
 
     Phase phase_ = Phase.COMMANDING;
     int num_;
@@ -22,21 +40,25 @@ public class Mauriçio : MonoBehaviour
     bool decided_ = false;
     public float offSet_;
 
+    float[] actPatron_;
+
     void Update()
     {
+        textPhase_.text = phase_.ToString();
         switch(phase_)
         {
             case Phase.COMMANDING:
                 if(!decided_)
                 {
-                    num_ = Random.Range(1, 6);
-                    numleft_ = num_;
                     decided_ = true;
+                    actPatron_ = Patrones.Patron.Ataques1[Random.Range(0, 3)];
+                    num_ = actPatron_.Length;
+                    numleft_ = num_;
                 }
                 Command();
                 break;
             case Phase.PLAYER:
-
+                Player();
                 break;
             case Phase.BATTLE:
 
@@ -46,13 +68,35 @@ public class Mauriçio : MonoBehaviour
 
     void Player()
     {
+        timer_ += Time.deltaTime;
 
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            numleft_--;
+            arrowsPlayer_.Add(new Pair<int, float>(0, timer_));
+        } 
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            numleft_--;
+            arrowsPlayer_.Add(new Pair<int, float>(1, timer_));
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            numleft_--;
+            arrowsPlayer_.Add(new Pair<int, float>(2, timer_));
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            numleft_--;
+            arrowsPlayer_.Add(new Pair<int, float>(3, timer_));
+        }
+        if (numleft_ == 0) phase_ = Phase.BATTLE;
     }
 
     void Command()
     {
         timer_ += Time.deltaTime;
-        if (timer_ > (60000 / bpm_) / 1000)
+        if (timer_ > actPatron_[num_-numleft_])
         {
             int aux = Random.Range(0, 4);
             float ini = CalculateExt(num_);
@@ -62,27 +106,28 @@ public class Mauriçio : MonoBehaviour
                 case 0:
                     arrow = Instantiate(arrowU_, commandBar_);
                     arrow.transform.position = new Vector3(ini + (num_-numleft_)* offSet_, commandBar_.position.y, commandBar_.position.z);
-                    arrow.GetComponent<Arrow>().mauriçio_ = this;
                     break;
                 case 1:
                     arrow = Instantiate(arrowD_, commandBar_);
                     arrow.transform.position = new Vector3(ini + (num_ - numleft_) * offSet_, commandBar_.position.y, commandBar_.position.z);
-                    arrow.GetComponent<Arrow>().mauriçio_ = this;
                     break;
                 case 2:
                     arrow = Instantiate(arrowL_, commandBar_);
                     arrow.transform.position = new Vector3(ini + (num_ - numleft_) * offSet_, commandBar_.position.y, commandBar_.position.z);
-                    arrow.GetComponent<Arrow>().mauriçio_ = this;
                     break;
                 case 3:
                     arrow = Instantiate(arrowR_, commandBar_);
                     arrow.transform.position = new Vector3(ini + (num_ - numleft_) * offSet_, commandBar_.position.y, commandBar_.position.z);
-                    arrow.GetComponent<Arrow>().mauriçio_ = this;
                     break;
             }
+            arrowsMauricio_.Add(new Pair<int, float>(aux, timer_));
             timer_ = 0;
             numleft_--;
-            if (numleft_ == 0) phase_ = Phase.PLAYER;
+            if (numleft_ == 0)
+            {
+                phase_ = Phase.PLAYER;
+                numleft_ = num_;
+            }
         }
     }
 
@@ -92,3 +137,4 @@ public class Mauriçio : MonoBehaviour
     }
     
 }
+
---
+++ Assets/Scripts/Patrones.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+namespace Patrones
+{
+    class Patron
+    {
+        static float[] Basico = { 0.5f, 1f, 1.5f };
+        static float[] Basico2 = { 0.3f, 0.4f, 1.0f };
+        static float[] Basico3 = { 1f, 1.5f, 1.7f };
+
+        public static float[][] Ataques1 = { Basico, Basico2, Basico3 };
+    }
+    
+}
