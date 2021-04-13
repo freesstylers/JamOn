cd3e3b4f8285f898657779faf6b0df90fd783ec6
javi <javari02@ucm.es>
2021-04-13 21:06:24 +0200

    Menú Principal rancio y barra que se mueve

---
+++ Assets/Scripts/SlidingBar.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingBar : MonoBehaviour
+{
+    public Transform bar;
+
+    public bool sliding = false;
+
+    public float slideTime = 2.0f;
+
+    float timer = 0.0f;
+
+    public float edgeOffset;
+
+    Vector3 startPos;
+    Vector3 endPos;
+
+    private void Start()
+    {
+        float wHalf = gameObject.GetComponent<SpriteRenderer>().sprite.bounds.extents.x - edgeOffset;
+
+        startPos = new Vector3(-wHalf, 0, 0);
+        endPos = new Vector3(wHalf, 0, 0);
+
+        if(bar)
+            bar.localPosition = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sliding)
+        {
+            if (timer <= slideTime)
+            {
+                bar.localPosition = Vector3.Lerp(startPos, endPos, timer / slideTime);
+
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                bar.localPosition = endPos;
+                sliding = false;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+            StartSlide();
+    }
+
+    public void StartSlide()
+    {
+        sliding = true;
+        timer = 0;
+
+        if (bar)
+            bar.localPosition = startPos;
+    }
+}
