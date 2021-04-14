5342dfa49b12317135162f36be479ea02f8fb4d1
ErGuille33 <37537142+ErGuille33@users.noreply.github.com>
2021-04-14 15:34:39 +0200

    Voz de jose, musica y saltitos

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] minion;
+
+    float timeElapsed;
+    float time;
+
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed >= time)
+        {
+            timeElapsed = 0;
+            SelectRandomClip();
+            source.Play();
+            SelectRandomTime();
+        }
+    }
+
+    void SelectRandomClip()
+    {
+        source.clip = minion[Random.Range(0, minion.Length)];
+    }
+
+    void SelectRandomTime()
+    {
+        time = Random.Range(5, 20);
+    }
+}
--- Assets/Scripts/MoveAround.cs
+++ Assets/Scripts/MoveAround.cs
@@ -8,24 +8,57 @@ public class MoveAround : MonoBehaviour
     public float intervaloX, intervaloY;
     public float minTime, maxTime;
 
+    bool jumpin = false;
+    bool sideForce = false;
+
+    float lastXPos;
+    float lastAltitude;
     float time;
     float actualTime = 0;
+    
     Vector2 fin;
     Vector2 ini;
     SpriteRenderer sprite;
+    Rigidbody2D rb;
+    
 
     void Start()
     {
         ini = new Vector2(transform.position.x, transform.parent.position.y);
         sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
         NewPosition();
     }
 
     void Update()
     {
-        sprite.sortingOrder = (int)(-transform.position.y * 10);
-        actualTime += Time.deltaTime;
-        transform.position = Vector2.Lerp(ini, fin, actualTime / time);
+        
+        if (!jumpin && !sideForce)
+        {
+            MoveMinionsWithKey();
+            sprite.sortingOrder = (int)(-transform.position.y * 10);
+            actualTime += Time.deltaTime;
+            transform.position = Vector2.Lerp(ini, fin, actualTime / time);
+        }
+        else if (sideForce && transform.position.y <= lastAltitude)
+        {
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            ini = new Vector2(transform.position.x, transform.position.y);
+            fin = new Vector2(lastXPos, transform.position.y);
+            actualTime = 0;
+            NewPosition();
+            lastAltitude = transform.position.y;
+            lastXPos = transform.position.x;
+            sideForce = false;
+        }
+        else if(jumpin && transform.position.y <= lastAltitude)
+        { 
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            lastAltitude = transform.position.y;
+            jumpin = false;
+ 
+        }
+        
         if (actualTime >= time)
         {
             actualTime = 0;
@@ -45,5 +78,35 @@ public class MoveAround : MonoBehaviour
     void NewTime()
     {
         time = Random.Range(minTime, maxTime);
+    }
+
+    void MoveMinionsWithKey()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            lastAltitude = transform.position.y;
+            rb.velocity = new Vector2(0,0);
+            rb.AddForce(new Vector2(0,3), ForceMode2D.Impulse);
+            jumpin = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            rb.velocity = new Vector2(0, 0);
+            lastAltitude = transform.position.y;
+            rb.AddForce(new Vector2(-3, 1), ForceMode2D.Impulse);
+            lastXPos = transform.position.x;
+            sideForce = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            rb.velocity = new Vector2(0, 0);
+            lastAltitude = transform.position.y;
+            rb.AddForce(new Vector2(3, 1), ForceMode2D.Impulse);
+            lastXPos = transform.position.x;
+            sideForce = true;
+        }
     }
 }
