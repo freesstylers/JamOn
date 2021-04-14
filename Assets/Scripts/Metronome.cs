087bead8b3b32c421475f9b9ac51919a6ac5da1b
PabloGarciaGrossi <pabgar20@ucm.es>
2021-04-14 16:39:43 +0200

    animacion

---
+++ Assets/Scripts/Metronome.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour
+{
+    public float bpm;
+    private float _actualTime;
+    private float _timer;
+
+    private Vector3 pointA;
+    private Vector3 pointB;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
