ed8ab4401ad9760d8ef6f4d6fde93d5f6a3493e7
PabloGarciaGrossi <pabgar20@ucm.es>
2021-04-14 18:47:55 +0200

    Metrónomo

--- Assets/Scripts/Metronome.cs
+++ Assets/Scripts/Metronome.cs
@@ -15,12 +15,23 @@ public class Metronome : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _actualTime = (bpm / 60);
+        pointA = transform.localEulerAngles + new Vector3(0f, 0f, 60f);
+        pointB = transform.localEulerAngles + new Vector3(0f, 0f, -60f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _timer += Time.deltaTime;
+        _actualTime = (bpm / 60);
+        if (_timer >= (1 / _actualTime))
+        {
+            //GetComponent<AudioSource>().Play();
+            //Debug.Log("Sound");
+            _timer = 0f;
+        }
+        float time = Mathf.PingPong(Time.time * _actualTime, 1);
+        transform.localEulerAngles = Vector3.Lerp(pointA, pointB, time);
     }
 }
