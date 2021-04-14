5cb4a452db5d3020aff6501a81cde66996ef8c2e
IgnOry <iory@ucm.es>
2021-04-14 22:14:34 +0200

    Input al ritmo, y las notas también

--- Assets/Scripts/Patrones.cs
+++ Assets/Scripts/Patrones.cs
@@ -9,8 +9,9 @@ namespace Patrones
         static float[] Basico = { 0.5f, 1f, 1.5f };
         static float[] Basico2 = { 0.3f, 0.4f, 1.0f };
         static float[] Basico3 = { 1f, 1.5f, 1.7f };
+        static float[] Basico4 = { 0.57f,  1.14f, 1.71f };
 
-        public static float[][] Ataques1 = { Basico, Basico2, Basico3 };
+        public static float[][] Ataques1 = { Basico, Basico2, Basico3, Basico4 };
     }
     
 }
