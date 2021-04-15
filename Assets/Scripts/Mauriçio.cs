c02c180b955e8271486cdf61fd802659cb634637
IgnOry <iory@ucm.es>
2021-04-15 02:01:03 +0200

    jaja si

--- Assets/Scripts/Mauriçio.cs
+++ Assets/Scripts/Mauriçio.cs
@@ -40,11 +40,12 @@ public class Mauriçio : MonoBehaviour
 
     float[] actPatron_;
 
-    //Cpsas gestion de input
+    float CommandTime;
+
+    //Cosas gestion de input
     float delay = -0.5f;
     float margin = 0.3f;
 
-    int fallos = 0;
     int inputsDone = 0;
 
     void Update()
@@ -55,7 +56,7 @@ public class Mauriçio : MonoBehaviour
 
         timer_ += Time.deltaTime;
 
-        if (timer_ >= 0)
+        if (timer_ >= 0.0f)
             commandBar_.gameObject.GetComponent<SlidingBar>().UpdateSlidePosition(timer_);
 
         switch (phase)
@@ -65,7 +66,7 @@ public class Mauriçio : MonoBehaviour
                 {
                     decided_ = true;
                     //actPatron_ = Patrones.Patron.Ataques1[Random.Range(0, 3)];
-                    actPatron_ = Patrones.Patron.Ataques1[3];
+                    actPatron_ = Patrones.Patron.Ataques1[0];
                     num_ = actPatron_.Length;
                     numleft_ = num_;
 
@@ -156,7 +157,7 @@ public class Mauriçio : MonoBehaviour
     {
         if (numleft_ > 0 && timer_ >= actPatron_[num_-numleft_])
         {
-            int aux = Random.Range(0, 4);
+            int aux = Random.Range(0, 1);
             GameObject arrow;
             switch (aux)
             {
@@ -188,124 +189,6 @@ public class Mauriçio : MonoBehaviour
             timer_ = delay; //Reset para poder hacer inputs a cholon
             commandBar_.gameObject.GetComponent<SlidingBar>().UpdateSlidePosition(2.0f);
         }
-    }
-    /*
-    void compareInputToOrder()
-    {
-        //Inicializacion y cosas auxiliares
-        bool[] alreadyChecked = new bool[arrowsPlayer_.Count];
-
-        for (int k = 0; k < arrowsPlayer_.Count; k++)
-        {
-            alreadyChecked[k] = false;
-        }
-
-        for (int i = 0; i < arrowsMauricio_.Count; i++)
-        {
-            //Compruebo inputs con margen dado en parametro (0.3 como inicial)
-            for (int j = 0; j < arrowsPlayer_.Count; j++)
-            {
-                if (alreadyChecked[j] != true) //Si ya se ha marcado como fallada, da igual mirarla o no
-                {
-                    float distance = Mathf.Abs(arrowsPlayer_[j].Second - arrowsMauricio_[i].Second);
-
-                    if (distance <= margin) //Está dentro
-                    {
-                        if (arrowsPlayer_[j].First == arrowsMauricio_[i].First) //Tecla correcta
-                        {
-                            Pair<Pair<int, float>, Pair<float, int>> pair = new Pair<Pair<int, float>, Pair<float, int>>();
-                            pair.First = arrowsPlayer_[j];
-                            pair.Second = new Pair<float, int>();
-                            pair.Second.First = distance;
-                            pair.Second.Second = j;
-                            aux.Add(pair);
-                        }
-                        else //Tecla erronea
-                        {
-                            if (alreadyChecked[j] != true) //Si ya es true, no hace falta mirar mas
-                                checkIfSuitableForNextOne(ref alreadyChecked, arrowsPlayer_[j], arrowsMauricio_[i], j, i);
-                        }
-                    }
-                    else //Está fuera
-                    {
-                        if (alreadyChecked[j] != true) //Si ya es true, no hace falta mirar mas
-                            checkIfSuitableForNextOne(ref alreadyChecked, arrowsPlayer_[j], arrowsMauricio_[i], j, i);
-                    }
-                }
-            }
-
-            if (aux.Count > 0)
-            {
-                int best = -1;
-                float minDistance = Mathf.Infinity;
-
-                //Cuando ya se han recorrido todas, se recorren las posibles buscando la distancia minima
-                for (int l = 0; l < aux.Count; l++)
-                {
-                    if (aux[l].Second.First < minDistance)
-                    {
-                        minDistance = aux[l].Second.First;
-                        best = l;
-                    }
-                }
-
-                for (int z = 0; z < best; z++) //Previas a la que tiene distancia minima
-                {
-                    alreadyChecked[aux[z].Second.Second] = true;
-
-                    fallos++;
-                }
-
-                for (int x = best + 1; x < aux.Count; x++) //Siguientes a la distancia minima //No se si el +1 va ahi
-                {
-                    if (alreadyChecked[aux[x].Second.Second] != true) //Si ya es true, no hace falta mirar mas
-                        checkIfSuitableForNextOne(ref alreadyChecked, aux[x].First, arrowsMauricio_[i], aux[x].Second.Second, i);
-                }
-
-                //Sumar puntos por el acierto (FORMULA DE CALCULO DE PUNTOS)
-                alreadyChecked[aux[best].Second.Second] = true;
-                //NO FALLO
-
-                aux.Clear(); //Para resetear de cara a la siguiente iteración
-            }
-        }
-    }*/
-
-    /*
-    void checkIfSuitableForNextOne(ref bool[] alreadyChecked, Pair<int, float> player, Pair<int, float> Mauriçio, int itPlayer, int itMauriçio)
-    {
-        if (itMauriçio == arrowsMauricio_.Count - 1) //Si ya es la ultima nota, error
-        {
-            alreadyChecked[itPlayer] = true;
-            fallos++;
-        }
-        else
-        {
-            float distanceAux = Mathf.Abs(arrowsPlayer_[itPlayer].Second - arrowsMauricio_[itMauriçio + 1].Second);
-
-            if (distanceAux > margin) //No puede contarse para la siguiente nota tampoco, asi que a contarlo como fallo
-            {
-                alreadyChecked[itPlayer] = true;
-                fallos++;
-            }
-        }
-    }
-
-    void checkAlreadyChecked(ref bool[] array, int valueToCheck)
-    {
-        if (array[valueToCheck] != true)
-        {
-            array[valueToCheck] = true;
-
-            fallos++;
-        }
-    }
-    */
-    /*
-    float CalculateExt(int num)
-    {
-        return -((num-1)*offSet_)/2;
-    }*/
-    
+    }    
 }
 
--- Assets/Scripts/Patrones.cs
+++ Assets/Scripts/Patrones.cs
@@ -1,17 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 namespace Patrones
 {
     class Patron
     {
-        static float[] Basico = { 0.5f, 1f, 1.5f };
+        static float[] TiemposPosibles = {2.0f, 3.0f, 4.0f, 5.0f};
+
+        static float[] Basico = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f};
         static float[] Basico2 = { 0.3f, 0.4f, 1.0f };
         static float[] Basico3 = { 1f, 1.5f, 1.7f };
         static float[] Basico4 = { 0.57f,  1.14f, 1.71f };
 
-        public static float[][] Ataques1 = { Basico, Basico2, Basico3, Basico4 };
+        public static float[][] Ataques1 = {Basico, Basico2, Basico3, Basico4 };
+        
+        public static float[] getArray (int tam)
+        {
+            float[] array = new float[tam];
+            array = Ataques1[Random.Range(0, Ataques1.Length)];
+            return array;
+        }
+        public static Pair<float[], float> makePair(float[] array, float value)
+        {
+            Pair<float[], float> test = new Pair<float[], float>();
+            int tam = Random.Range(2, 5);
+            test.First = new float[tam];
+
+            test.First = getArray(tam);
+
+            test.Second = Random.Range(0, TiemposPosibles.Length);
+
+            return test;
+        }
     }
-    
 }
