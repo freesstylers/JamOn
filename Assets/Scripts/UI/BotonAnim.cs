ac200562cb7b23843e61d9e4b923f263f02545c9
Álvar de Diego López <alvarded@ucm.es>
2021-04-16 13:21:06 +0200

    Anim UI

---
+++ Assets/Scripts/UI/BotonAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonAnim : MonoBehaviour
+{
+
+    public LeanTweenType easyType;
+
+    void Start()
+    {
+        transform.localScale = Vector3.zero;
+
+        LeanTween.rotate(gameObject, new Vector3(0, 0, 720), 1.4f).setEase(easyType).setDelay(0.3f);
+        LeanTween.scale(gameObject, new Vector3(1f, 1f, 1), 1f).setEase(easyType).setDelay(0.3f);
+    }
+
+    
+}
