65b86814ff4fae0ca1d6e41c5303a824a6ff87f0
javi <javari02@ucm.es>
2021-04-17 01:40:45 +0200

    jonica

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public Button[] buttons;
+    public GameObject optionsCanvas;
+
+    public void Toggle()
+    {
+        if(optionsCanvas)
+            optionsCanvas.SetActive(!optionsCanvas.activeSelf);
+
+        foreach (Button b in buttons)
+            b.interactable = !b.interactable;
+    }
+}
