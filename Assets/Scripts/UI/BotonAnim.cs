eaaba88e8cf83805d2041e12ea9738f637c6586a
Álvar de Diego López <alvarded@ucm.es>
2021-04-16 14:14:21 +0200

    countdown

--- Assets/Scripts/UI/BotonAnim.cs
+++ Assets/Scripts/UI/BotonAnim.cs
@@ -6,14 +6,24 @@ public class BotonAnim : MonoBehaviour
 {
 
     public LeanTweenType easyType;
+    public GameObject countDown;
 
     void Start()
     {
         transform.localScale = Vector3.zero;
 
-        LeanTween.rotate(gameObject, new Vector3(0, 0, 720), 1.4f).setEase(easyType).setDelay(0.3f);
         LeanTween.scale(gameObject, new Vector3(1f, 1f, 1), 1f).setEase(easyType).setDelay(0.3f);
+        LeanTween.rotate(gameObject, new Vector3(0, 0, 720), 1.4f).setEase(easyType).setDelay(0.3f);
     }
 
+    public void Resume()
+    {
+        LeanTween.scale(gameObject, new Vector3(0f, 0f, 1), 0.3f).setOnComplete(ResumeEnd);
+    }
+
+    public void ResumeEnd()
+    {
+        countDown.SetActive(true);
+    }
     
 }
---
+++ Assets/Scripts/UI/Countdown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    public GameObject one, two, three;
+
+    private void OnEnable()
+    {
+        one.transform.localScale = Vector3.zero;
+        two.transform.localScale = Vector3.zero;
+        three.transform.localScale = Vector3.zero;
+
+        LeanTween.scale(three, new Vector3(1f, 1f, 1), 0.4f).setOnComplete(ResetThree);      
+    }
+
+    void ScaleTwo()
+    {
+        LeanTween.scale(two, new Vector3(1f, 1f, 1), 0.4f).setOnComplete(ResetTwo);       
+    }
+
+    void ScaleOne()
+    {
+        LeanTween.scale(one, new Vector3(1f, 1f, 1), 0.4f);
+    }
+
+    void ResetOne()
+    {
+        one.transform.localScale = Vector3.zero;
+    }
+    void ResetTwo()
+    {
+        LeanTween.scale(two, new Vector3(0f, 0f, 1), 0.1f).setDelay(0.4f).setOnComplete(ScaleOne);
+    }
+    void ResetThree()
+    {
+        LeanTween.scale(three, new Vector3(0f, 0f, 1), 0.1f).setDelay(0.4f).setOnComplete(ScaleTwo);
+    }
+}
